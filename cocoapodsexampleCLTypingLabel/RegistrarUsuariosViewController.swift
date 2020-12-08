dfb53275ef52b8fca78cc93cbffd36cbe4ecca36
Catalina <catalina@iMac-de-Catalina.local>
2020-12-08 15:59:38 -0600

    Constrains

---
+++ cocoapodsexampleCLTypingLabel/RegistrarUsuariosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrarUsuariosViewController.swift
+//  cocoapodsexampleCLTypingLabel
+//
+//  Created by Catalina on 08/12/20.
+//  Copyright © 2020 funcion. All rights reserved.
+//
+
+import UIKit
+
+class RegistrarUsuariosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
